c29694a226d8f1a32457ae1fdac8c8e75f579fa0
rohithreddybc <rohithreddybc98@gmail.com>
2023-12-15 02:29:18 -0500

    Product Of Array Except Self

--- ArraysAndStrings/src/com/rohith/leetcode75/arrayAndString/ProductOfArrayExceptSelf.java
+++ ArraysAndStrings/src/com/rohith/leetcode75/arrayAndString/ProductOfArrayExceptSelf.java
@@ -3,35 +3,31 @@ package com.rohith.leetcode75.arrayAndString;
 import java.util.Arrays;
 
 public class ProductOfArrayExceptSelf {
-    public int[] productExceptSelf(int[] nums) {
-        int n = nums.length;
-        int ans[] = new int[n];
-        Arrays.fill(ans, 1);
+	public int[] productExceptSelf(int[] nums) {
 
-        // Forward pass
-        int curr = 1;
-        for (int i = 0; i < n; i++) {
-            ans[i] *= curr;
-            curr *= nums[i];
-        }
+		int preProd = 1;
+		int postProd = 1;
 
-        // Backward pass
-        curr = 1;
-        for (int i = n - 1; i >= 0; i--) {
-            ans[i] *= curr;
-            curr *= nums[i];
-        }
+		int[] outputArr = new int[nums.length];
 
-        return ans;
-    }
+		for (int i = 0; i <= nums.length - 1; i++) {
+			outputArr[i] = preProd;
+			preProd *= nums[i];
+		}
+		for (int i = nums.length - 1; i >= 0; i--) {
+			outputArr[i] *= postProd;
+			postProd *= nums[i];
+		}
+		return outputArr;
+	}
 
-    public static void main(String[] args) {
-        // Test the solution with example input
-    	ProductOfArrayExceptSelf solution = new ProductOfArrayExceptSelf();
-        int[] input = {1, 2, 3, 4};
-        int[] result = solution.productExceptSelf(input);
+	public static void main(String[] args) {
+		// Test the solution with example input
+		ProductOfArrayExceptSelf solution = new ProductOfArrayExceptSelf();
+		int[] input = { 1, 2, 3, 4 };
+		int[] result = solution.productExceptSelf(input);
 
-        // Print the result
-        System.out.println(Arrays.toString(result));
-    }
+		// Print the result
+		System.out.println(Arrays.toString(result));
+	}
 }
